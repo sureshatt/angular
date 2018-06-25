d2cf4164d7d3f768af5912abc045660a87e76c64
sureshatt <suresh.attanayake@gmail.com>
2018-06-25 22:55:06 +0200

    heroes: observable

--- angular-heroes-tutorial/src/app/heros/heros.component.ts
+++ angular-heroes-tutorial/src/app/heros/heros.component.ts
@@ -29,7 +29,8 @@ export class HerosComponent implements OnInit {
   }
 
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
 }
